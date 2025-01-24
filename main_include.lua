b056a15424f97d938859195bcee1c6885b1d4509
Sooly890 <solomontnz@gmail.com>
2025-01-24 17:48:56 +1300

    Remove unnecessary Include_main

--- main_include.lua
+++
@@ -1,6 +0,0 @@
-return function()
-    externalincludedirs{ "Logging/include", "Logging/rang/include" }
-    links { "Logging" }
-    filter { "system:linux", "action:gmake2" }
-        links { "stdc++exp" }
-end
